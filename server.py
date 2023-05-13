c5770be40bff5caef7c44b68ed3417a6b8ab28e2
dowlan22 <132970241+dowlan22@users.noreply.github.com>
2023-05-14 02:06:57 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ port = int(os.getenv("PORT", 8080))
 users = {}
 
 # 用于验证用户的装饰器
-def auth_required(f):
+def requires_auth(f):
     def wrapper(*args, **kwargs):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
@@ -20,34 +20,30 @@ def auth_required(f):
         return f(*args, **kwargs)
     return wrapper
 
-# 检查用户名和密码是否匹配
-def check_auth(username, password):
-    return username in users and users[username] == password
-
 # 设置用户名和密码
 @app.route('/users/<username>', methods=['PUT'])
-@auth_required
+@requires_auth
 def add_user(username):
     users[username] = request.json['password']
     return jsonify({"message": "User added successfully"})
 
 # 删除用户
 @app.route('/users/<username>', methods=['DELETE'])
-@auth_required
+@requires_auth
 def delete_user(username):
     users.pop(username, None)
     return jsonify({"message": "User deleted successfully"})
 
 # 获取所有用户
 @app.route('/users', methods=['GET'])
-@auth_required
+@requires_auth
 def get_users():
     return jsonify(users)
 
 # HTTP/HTTPS代理请求处理
-@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST'])
-@auth_required
+@requires_auth
 def proxy(path):
     url = request.url.replace(request.host_url, '', 1)
     if request.is_secure:
@@ -72,7 +68,7 @@ def proxy(path):
 
 # SOCKS5代理请求处理
 @app.route('/socks', methods=['GET', 'POST'])
-@auth_required
+@requires_auth
 def socks_proxy():
     socks.set_default_proxy(
         socks.SOCKS5,
